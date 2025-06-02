f19aef0e0834ee5ca0a1cb79dcf3ff4934602a84
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-06-02 19:50:27 +0200

    Rutas en React  - entrada principal

--- productos-front/src/App.js
+++ productos-front/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import ProductosLista from './components/ProductosLista';
+import ProductoForm from './components/ProductoForm';
 
 function App() {
+  const isAuthenticated = !!localStorage.getItem('token');
+
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {isAuthenticated ? (
+          <>
+            <Route path="/productos" element={<ProductosLista />} />
+            <Route path="/productos/nuevo" element={<ProductoForm />} />
+            <Route path="/productos/editar/:id" element={<ProductoForm />} />
+            <Route path="*" element={<Navigate to="/productos" />} />
+          </>
+        ) : (
+          <Route path="*" element={<Navigate to="/login" />} />
+        )}
+      </Routes>
+    </Router>
   );
 }
 
