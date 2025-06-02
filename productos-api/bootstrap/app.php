c14a65b98118c1ac8129c1777780fa0f21be3555
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-06-02 19:37:12 +0200

    añadir exceptions

--- productos-api/bootstrap/app.php
+++ productos-api/bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,10 +15,23 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'auth'       => \App\Http\Middleware\Authenticate::class,
-            'auth:api'   => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
-            ])
+            'auth'     => \App\Http\Middleware\Authenticate::class,
+            'auth:api' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        $exceptions->respond(function (Response $response, Throwable $e) {
+            if ($e instanceof ModelNotFoundException) {
+                return response()->json([
+                    'error' => 'Recurso no encontrado',
+                    'codigo' => 404,
+                    'meta' => [
+                        'version' => '1.0',
+                        'timestamp' => now()->toISOString()
+                    ]
+                ], 404);
+            }
+            return $response;
+        });
+    })
+    ->create();
