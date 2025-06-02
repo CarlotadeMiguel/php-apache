1bb22d56123f1ae33934908d0cca41f094e07782
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-06-02 19:39:29 +0200

    añadir rutas publicas

--- productos-api/routes/api.php
+++ productos-api/routes/api.php
@@ -5,14 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\AuthController;
 
-// Rutas públicas para login
+// Rutas públicas
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('productos', [ProductoController::class, 'index']);
+Route::get('productos/{producto}', [ProductoController::class, 'show']);
+
 
 // Rutas protegidas por JWT
 Route::middleware('auth:api')->group(function () {
-    // apiResource crea index, store, show, update, destroy
-    Route::apiResource('productos', ProductoController::class);
-    // Para PATCH, Laravel mapea update si recibe PATCH. 
-    // Si quisieras un método separado 'patch', puedes:
-    // Route::patch('productos/{producto}', [ProductoController::class, 'patch']);
+    Route::apiResource('productos', ProductoController::class)->except(['index', 'show']);
 });
