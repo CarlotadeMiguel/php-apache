641b574ed4f407f5595286452ccf853abcedd43c
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-06-03 21:01:23 +0200

    cors

--- productos-api/bootstrap/app.php
+++ productos-api/bootstrap/app.php
@@ -10,10 +10,13 @@ use Throwable;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        
         $middleware->alias([
             'auth'     => \App\Http\Middleware\Authenticate::class,
             'auth:api' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
---
+++ productos-api/config/cors.php
@@ -0,0 +1,20 @@
+<?php
+// config/cors.php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    
+    'allowed_methods' => ['*'],
+    
+    'allowed_origins' => ['*'], // En producción cambia a dominios específicos
+    
+    'allowed_origins_patterns' => [],
+    
+    'allowed_headers' => ['*'],
+    
+    'exposed_headers' => [],
+    
+    'max_age' => 0,
+    
+    'supports_credentials' => false,
+];
